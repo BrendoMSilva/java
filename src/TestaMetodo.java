9b2a1295bd69877c349fdd5a733b2d8e8c6a2bbe
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-02 16:31:59 -0300

    update

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -21,5 +21,9 @@ public class TestaMetodo {
         }
         System.out.println(contaDaMarcela.saldo);
         System.out.println(contaDoPaulo.saldo); 
+        
+        
+        contaDoPaulo.titular = "paulo silveira";
+        System.out.println(contaDoPaulo.titular);
     }
 }
