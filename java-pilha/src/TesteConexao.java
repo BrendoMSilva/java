027271997b29f8335c8c37640d708e241d70c668
BrendoSilva <brendosilvaxxt@gmail.com>
2022-01-16 16:22:19 -0300

    java.lang
    praticando com o lang

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -1,26 +1,32 @@
-public class TesteConexao {
-	
-		public static void main(String[] args) {
 
-		    try (Conexao conexao = new Conexao()) {
-		        conexao.leDados();
-		    } catch(IllegalStateException ex) {
-		        System.out.println("Deu erro na conexao");
-		    }
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		
+		
+		try (Conexao conexao = new Conexao() ) {
+			conexao.leDados();
+		} catch(IllegalStateException ex) {
+			System.out.println("Deu erro na conexao");
 		}
+		
+		
+		//-----------------------------
+		
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch(IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao");
+//		} finally {
+//			System.out.println("finally");
+//			if(con != null) {
+//				con.close();
+//			}
+//		}
+		
+
+	}
+
 }
-		
-		
-		
-	//	-------------------------------------
-//	    Conexao con = null;
-//	    try {
-//	        con = new Conexao();
-//	        con.leDados();
-//	    } catch(IllegalStateException ex) {
-//	        System.out.println("Deu erro na conexao");
-//	    } finally {
-//	        con.fecha();
-//	    }
-//	}
-//}
